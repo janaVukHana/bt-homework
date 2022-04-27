93aa6df7aee4611727fd9de0108da817747c8a92
Ilija Radovanovic <ilija009@gmail.com>
2022-04-28 01:51:09 +0200

    hw10 expanded

--- hw10.php
+++ hw10.php
@@ -3,6 +3,8 @@
 // zadatak 1
 echo 'ZADATAK BR.1';
 echo '<br>';
+echo 'First way';
+echo '<br>';
 
 $num_1 = 150;
 $num_2 = 90;
@@ -17,6 +19,20 @@ echo $num_1 / $num_2;
 echo '<br>';
 
 echo $num_1 * $num_2;
+echo '<br>';
+
+// drugi nacin
+echo 'Second way';
+echo '<br>';
+$num_arr = array($num_1, $num_2);
+$sum = array_sum($num_arr);
+echo 'Sum: ' . $sum;
+echo '<br>';
+echo 'Subtract: ' . bcsub($num_1, $num_2);
+echo '<br>';
+echo 'Divide: ' . intdiv($num_1, $num_2);
+echo '<br>';
+echo 'Multiply: ' . bcmul($num_1, $num_2);
 echo '<br>';
 
 // zadatak 2
@@ -45,16 +61,24 @@ echo '<br>';
 // zadatak 3
 echo 'ZADATAK BR.3';
 echo '<br>';
+echo 'First way';
+echo '<br>';
 $a = 9;
 $b = 1;
 $c = 5;
 
 $total = $a + $b + $c;
 
-echo "Zbir brojeva $a, $b, i $c je $total."
+echo "Zbir brojeva $a, $b, i $c je $total.";
+echo '<br>';
 
-
-
+echo 'Second way';
+echo '<br>';
+$a = mt_rand(0, 10);
+$b = mt_rand(0, 10);
+$c = mt_rand(0, 10);
+$rand_num_arr = array($a, $b, $c);
+echo 'Total sum of $a, $b and $c is: ' . array_sum($rand_num_arr);
 
 
 ?>
