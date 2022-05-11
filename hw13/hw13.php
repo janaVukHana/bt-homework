2414cdde2692e7c6770554422dc22258775e7a39
Ilija Radovanovic <ilija009@gmail.com>
2022-05-11 14:49:17 +0200

    modifies strange_surface func

--- hw13/hw13.php
+++ hw13/hw13.php
@@ -8,12 +8,12 @@ function circle_surface($r) {
     return pow($r, 2) * pi();
 }
 
-function strange_shape_surface($a, $b, $r) {
-    return rectangle_surface($a, $b) + (circle_surface($r) / 2);
+function strange_shape_surface($a, $b) {
+    return rectangle_surface($a, $b) + (circle_surface($b) / 2);
 }
 
 function add_three_shapes_surface($a, $b, $r, $c, $d) {
-    return rectangle_surface($a, $b) + circle_surface($r) + strange_shape_surface($c, $d, $r);
+    return rectangle_surface($a, $b) + circle_surface($r) + strange_shape_surface($c, $d);
 }
 
 echo 'RECTANGLE SURFACE: ' . rectangle_surface(2, 3) . '<br>';
