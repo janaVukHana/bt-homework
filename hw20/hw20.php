0497a5589eb77f6db992ddf9d7a0bbece90f0f01
Ilija Radovanovic <ilija009@gmail.com>
2022-06-01 15:45:14 +0200

    change func displayMe() to abstract

--- hw20/hw20.php
+++ hw20/hw20.php
@@ -1,6 +1,15 @@
 <?php
+// PITANJE: ako u class Contact napravim 
+    // protected function __construct($phone, $email) {
+    //     $this->phone = $phone;
+    //     $this->email = $email;
+    // }
+    // i onda u sledece dve klase probam u __construct funkciji da uradim 
+    // parent::__construct($phone, $email) kod mi ne radi.
+    // Zashto?
 
 
+// CONTACT CLASS //////////////////////////////////////////////////////////////////////////////
 abstract class Contact {
     protected $phone;
     protected $email;
@@ -10,7 +19,7 @@ abstract class Contact {
      * @param string $phone
      * @return void
      */
-    public function setPhone(string $phone):void {
+    protected function setPhone(string $phone):void {
         $this->phone = $phone;
     }
 
@@ -19,7 +28,7 @@ abstract class Contact {
      * @param string $email
      * @return void
      */
-    public function setEmail(string $email):void {
+    protected function setEmail(string $email):void {
         $this->email = $email;
     }
 
@@ -28,7 +37,7 @@ abstract class Contact {
      * 
      * @return string
      */
-    public function getPhone():string {
+    protected function getPhone():string {
         return $this->phone;
     }
 
@@ -37,7 +46,7 @@ abstract class Contact {
      * 
      * @return string
      */
-    public function getEmail():string {
+    protected function getEmail():string {
         return $this->email;
     }
 
@@ -46,39 +55,24 @@ abstract class Contact {
      * @param string $text 
      * @return bool
      */
-    public function contains(string $text = ''):bool {
+    protected function contains(string $text = ''):bool {
         if(str_contains($this->phone, $text) || str_contains($this->email, $text)) {
             return true;
         }
         return false;
     }
 
-    // U klasi Contact dodati funkciju displayMe() koja ispisuje vrednosti atributa. 
     /**
      * This function echo attributes values
      * @return void
      */
-    public function displayMe() {
-        echo "Phone: $this->phone. Email: $this->email.";
-    }
+    // public function displayMe() {
+    //     echo "Phone: $this->phone. Email: $this->email.";
+    // }
+    abstract protected function displayMe();
  }
 
-// $contact1 = new Contact('062123456', 'test@test.com');
-// $contact1->setPhone('062421903');
-// $contact1->setEmail('ilija009@gmail.com');
-
-// $contact2 = new Contact('062123457', 'test2@test.com');
-// $contact2->setPhone('0642255544');
-// $contact2->setEmail('johndoe@gmail.com');
-
-// $contact3 = new Contact('062123458', 'test3@test.com');
-// $contact3->setPhone('060123123');
-// $contact3->setEmail('steva@gmail.com');
-
-// $contact1->displayMe();
-// Kreirati Klasu CustomerContact koja ima sve kao i klasa Contact 
-// kao i protected $firstName; protected $lastName; 
-// kao i metodu displayMe() (override) koja ispisuje sve atribute CustomerContact klase. 
+// CUSTOMER CONTACT CLASS ////////////////////////////////////////////////////////////////////////
 class CustomerContact extends Contact {
     protected $firstName;
     protected $lastName;
@@ -91,7 +85,6 @@ class CustomerContact extends Contact {
         $this->lastName = $lastName;
         $this->phone = $phone;
         $this->email = $email;
-        // parent::__construct($phone, $email);
     }
 
     /**
@@ -103,9 +96,8 @@ class CustomerContact extends Contact {
     }
 }
 
-// Kreirati Klasu BusinessContact koja ima sve kao i klasa Contact 
-// kao i protected $address; protected $companyName; 
-// kao i metodu displayMe() (override) koja ispisuje sve atribute BusinessContact klase. 
+ 
+// BUSINESS CONTACT CLASS /////////////////////////////////////////////////////////////////////////////////////
 class BusinessContact extends Contact {
     protected $address;
     protected $companyName;
@@ -115,7 +107,6 @@ class BusinessContact extends Contact {
         $this->companyName = $companyName;
         $this->phone = $phone;
         $this->email = $email;
-        // parent::__construct($phone, $email);
     }
 
     /**
@@ -126,16 +117,14 @@ class BusinessContact extends Contact {
         echo "$this->address $this->companyName $this->phone $this->email.<br>";
     }
 }
-// Postaviti da ne moze da se kreira objekat klase Contact, vec samo klase CustomerContact i BusinessContact.
 
-// Na kraju kreirati po 2 objekta CustomerContact i BusinessContact klasa. 
 $customer_contact_1 = new CustomerContact('Ilija', 'Radovanovic', '123456789', 'mail@gmail.com');
 $customer_contact_2 = new CustomerContact('Mornar', 'Popaj', '111333444', 'popaj@gmail.com');
 
 $business_contact_1 = new BusinessContact('Novi Sad', 'Nestle', '999888777', 'nestle@gmal.com');
-$business_contact_2 = new BusinessContact('Beograd', 'Snickers', '555555555', 'snicker@gmal.com');
+$business_contact_2 = new BusinessContact('Beograd', 'Snickers', '955555555', 'snicker@gmal.com');
 
-// Dodati ih u objekat ContactList i ispisati sve kontakte.
+// CONTACT LIST CLASS /////////////////////////////////////////////////////////////////////////////////////////////
 class ContactList {
     protected $contacts = [];
 
@@ -158,7 +147,6 @@ class ContactList {
         $all_contacts = $this->contacts;
 
         foreach($all_contacts as $contact) {
-            // echo $contact->getPhone() . ' ' . $contact->getEmail() . '<br>';
             $contact->displayMe();
         }
        
@@ -171,8 +159,3 @@ $contact_list->addContact($customer_contact_2);
 $contact_list->addContact($business_contact_1);
 $contact_list->addContact($business_contact_2);
 $contact_list->listAllContacts();
-
-
-// foreach($this->contacts as $contact) {
-// 	echo $contact->displayMe();
-// }
